4c1163f5144983dd67d6d92300a76e386fd3cb89
nathanwhit <nathan.whitaker01@gmail.com>
2020-01-06 18:28:30 -0500

    Remove dependency on thiserror; handwrite Display impl

--- src/errors.rs
+++ src/errors.rs
@@ -1,11 +1,23 @@
-use thiserror::{Error};
+use std::fmt;
 
-#[derive(Error, Debug)]
+#[derive(Debug)]
 pub enum MiniYamlError {
-    #[error("tags are disallowed in minimal-yaml")]
-    TagsDisallowed,
-    #[error("anchors are disallowed in minimal-yaml")]
-    AnchorsDisallowed,
-    #[error("aliases are disallowed in minimal-yaml")]
+    /// error produced when an alias is encountered in the parser input
     AliasesDisallowed,
+    /// error produced when an anchor is encountered in the parser input
+    AnchorsDisallowed,
+    /// error produced when a tag is encountered in the parser input
+    TagsDisallowed,
+}
+
+impl std::error::Error for MiniYamlError { }
+
+impl fmt::Display for MiniYamlError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", match self {
+            MiniYamlError::AliasesDisallowed => "aliases are disallowed in minimal-yaml",
+            MiniYamlError::AnchorsDisallowed => "anchors are disallowed in minimal-yaml",
+            MiniYamlError::TagsDisallowed => "tags are disallowed in minimal-yaml",
+        })
+    }
 }
