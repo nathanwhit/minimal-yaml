fba3b5517352cf055a638e04b61d5a5088a506cc
nathanwhit <nathan.whitaker01@gmail.com>
2020-01-11 21:05:30 -0500

    Add parsing error, add Span info to errors

--- src/errors.rs
+++ src/errors.rs
@@ -1,13 +1,16 @@
 use core::fmt;
+use crate::tokenize::Span;
 
 #[derive(Debug)]
 pub enum MiniYamlError {
     /// error produced when an alias is encountered in the parser input
-    AliasesDisallowed,
+    AliasesDisallowed(Span),
     /// error produced when an anchor is encountered in the parser input
-    AnchorsDisallowed,
+    AnchorsDisallowed(Span),
     /// error produced when a tag is encountered in the parser input
-    TagsDisallowed,
+    TagsDisallowed(Span),
+    /// error produced when parsing invalid Yaml
+    ParseError(Span),
 }
 
 impl std::error::Error for MiniYamlError {}
@@ -18,9 +21,10 @@ impl fmt::Display for MiniYamlError {
             f,
             "{}",
             match self {
-                MiniYamlError::AliasesDisallowed => "aliases are disallowed in minimal-yaml",
-                MiniYamlError::AnchorsDisallowed => "anchors are disallowed in minimal-yaml",
-                MiniYamlError::TagsDisallowed => "tags are disallowed in minimal-yaml",
+                MiniYamlError::AliasesDisallowed(..) => "aliases are disallowed in minimal-yaml",
+                MiniYamlError::AnchorsDisallowed(..) => "anchors are disallowed in minimal-yaml",
+                MiniYamlError::TagsDisallowed(..) => "tags are disallowed in minimal-yaml",
+                MiniYamlError::ParseError(..) => "parsing error occurred",
             }
         )
     }
