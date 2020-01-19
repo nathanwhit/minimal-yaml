566614e97f62a293ca75d0a7fc2360123d6f1c6a
nathanwhit <nathan.whitaker01@gmail.com>
2020-01-18 20:58:42 -0500

    Add tests for flow sequences

--- src/tests.rs
+++ src/tests.rs
@@ -16,7 +16,8 @@ macro_rules! mk_test {
         }
     };
 }
-}
+
+// Scalars
 
 mk_test!(
     double quote scalar whitespace;
@@ -46,4 +47,57 @@ mk_test!(
 mk_test!(
     no quote scalar no whitespace;
     "anunquoted_scalar_value_withoutwhitespace" => Scalar("anunquoted_scalar_value_withoutwhitespace")
+);
+
+use Scalar as S;
+
+macro_rules! seq {
+    ($($val: expr),*) => {
+        Sequence(vec![$( Scalar($val )),*])
+    };
+
+    ($($val: expr);*) => {
+        Sequence(vec![$( $val ),*])
+    }
+}
+
+// Flow Sequences
+
+mk_test!(
+    empty flow sequence;
+    r"[   ]" => seq!()
+);
+
+mk_test!(
+    one element flow sequence;
+    r"[   element]" => seq!("element")
+);
+
+mk_test!(
+    simple flow sequence no quotes;
+    r"[      a,      b     , c  ,d , e       ]" => seq!("a", "b", "c", "d", "e")
+);
+
+mk_test!(
+    simple flow sequence mixed quotes;
+    r#"[ "a", 'b' , "  c ", d, ' e  ' ]"# => seq!("a", "b", "  c ", "d", " e  ")
+);
+
+mk_test!(
+    multiple flow sequence no quotes;
+    r"[[ a, b, c ,d, e   ] , [ f, g, h, i , j ]]" => 
+        seq!(
+            seq!("a", "b","c", "d", "e");
+            seq!("f", "g", "h", "i", "j")
+        )
+);
+
+mk_test!(
+    mixed kind flow sequence no quotes;
+    r"[[ a, b, c], el]" => seq!(seq!("a", "b", "c"); S("el"))
+);
+
+mk_test!(
+    mixed kind flow sequence quotes;
+    r#"[" elem " , [ a, 'b ' , "   c "]]"# => seq!(S(" elem "); seq!("a", "b ", "   c "))
 );
