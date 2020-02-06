661ef5380de2786a1f665846c150197a43370c33
nathanwhit <nathan.whitaker01@gmail.com>
2020-02-06 12:20:20 -0500

    Add basic info to parse errors, edit Display impl

--- src/errors.rs
+++ src/errors.rs
@@ -1,6 +1,6 @@
 use core::fmt;
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub enum MiniYamlError {
     /// error produced when an alias is encountered in the parser input
     AliasesDisallowed,
@@ -9,22 +9,25 @@ pub enum MiniYamlError {
     /// error produced when a tag is encountered in the parser input
     TagsDisallowed,
     /// error produced when parsing invalid Yaml
-    ParseError,
+    ParseError(usize, usize, String),
 }
 
 impl std::error::Error for MiniYamlError {}
 
 impl fmt::Display for MiniYamlError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(
-            f,
-            "{}",
-            match self {
-                MiniYamlError::AliasesDisallowed => "aliases are disallowed in minimal-yaml",
-                MiniYamlError::AnchorsDisallowed => "anchors are disallowed in minimal-yaml",
-                MiniYamlError::TagsDisallowed => "tags are disallowed in minimal-yaml",
-                MiniYamlError::ParseError => "parsing error occurred",
-            }
-        )
+        match self {
+            MiniYamlError::ParseError(line, col, ..) => write!(f, "parsing error occurred at line {} col {}", line, col),
+            _ => write!(
+                f,
+                "{}",
+                match self {
+                    MiniYamlError::AliasesDisallowed => "aliases are disallowed in minimal-yaml",
+                    MiniYamlError::AnchorsDisallowed => "anchors are disallowed in minimal-yaml",
+                    MiniYamlError::TagsDisallowed => "tags are disallowed in minimal-yaml",
+                    _ => unreachable!()
+                }
+            )
+        }
     }
 }
